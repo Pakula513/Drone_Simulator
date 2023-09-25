83be4aa947b1360e974c89e246c11abc4578a261
Pakula513 <akula.prateek@gmail.com>
2023-09-25 17:43:34 -0400

    response to code_review comments except argparse

--- drone_test_1.py
+++ drone_test_1.py
@@ -4,7 +4,7 @@ from scipy import signal
 
 
 # rcCommand 0 is roll, 1 is pitch, 2 is yaw
-blackbox_df = pd.read_csv(r'C:\\Users\\Akula\\Documents\\Summer2023_Stuff\\06_23_2023_Data\\drone_test_1_BB.csv', skiprows = lambda x: x in range(141)
+blackbox_df = pd.read_csv('drone_test_1_BB.csv', skiprows = 141
                  ,usecols = ['rcCommand[0]', 'rcCommand[1]', 'rcCommand[2]', 'gyroADC[0]', 'gyroADC[1]', 'gyroADC[2]'])
 
 
@@ -32,6 +32,37 @@ filtered_gyroArray = np.hstack((filtered, filtered2, filtered3))
 
 
 
-np.save('Documents/Summer2023_Stuff/data/06_23_2023_x_Array', stickArray)
-np.save('Documents/Summer2023_Stuff/data/06_23_2023_y_Array', filtered_gyroArray)
+np.save('06_23_2023_x_Array', stickArray)
+np.save('06_23_2023_y_Array', filtered_gyroArray)
 
+
+
+def load_filter_bf_datafile(path, signal_order, signal_crit_freq, signal_sampling_freq, signal_output):
+    blackbox_df = pd.read_csv(path, skiprows = 141, 
+                            usecols = ['rcCommand[0]', 'rcCommand[1]', 'rcCommand[2]', 'gyroADC[0]', 'gyroADC[1]', 'gyroADC[2]'])
+    
+
+    pitchArray = np.array(blackbox_df.loc[:, 'rcCommand[1]'].to_numpy(), ndmin = 2).T
+    rollArray = np.array(blackbox_df.loc[:, 'rcCommand[0]'].to_numpy(), ndmin = 2).T
+    yawArray = np.array(blackbox_df.loc[:, 'rcCommand[2]'].to_numpy(), ndmin = 2).T
+    stickArray = np.hstack((pitchArray, rollArray, yawArray))
+
+
+
+    gyro_x = np.array(blackbox_df.loc[:, 'gyroADC[0]'].to_numpy(), ndmin = 2).T
+    gyro_y = np.array(blackbox_df.loc[:, 'gyroADC[1]'].to_numpy(), ndmin = 2).T
+    gyro_z = np.array(blackbox_df.loc[:, 'gyroADC[2]'].to_numpy(), ndmin = 2).T
+
+
+    sos = signal.butter(signal_order, signal_crit_freq, fs = signal_sampling_freq, output = signal_output)
+    filtered = signal.sosfilt(sos, gyro_x)
+    filtered2 = signal.sosfilt(sos, gyro_y)
+    filtered3 = signal.sosfilt(sos, gyro_z)
+
+
+    filtered_gyroArray = np.hstack((filtered, filtered2, filtered3))
+
+    return stickArray, filtered_gyroArray
+
+
+stickArray, filtered_gyroArray = load_filter_bf_datafile('drone_test_1_BB.csv', 7, 100, 500, 'sos')
--- impulse_plotter.py
+++ impulse_plotter.py
@@ -1,11 +1,56 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 from scipy import signal
 from scipy import linalg
 from scipy.signal import lfilter
+import matplotlib.pyplot as plt
 
-x = np.load('Documents/Summer2023_Stuff/data/06_23_2023_x_Array.npy')
-y = np.load('Documents/Summer2023_Stuff/data/06_23_2023_y_Array.npy')
+
+
+
+def load_filter_bf_datafile(path, signal_order, signal_crit_freq, signal_sampling_freq, signal_output):
+
+    # rcCommand 0 is roll, 1 is pitch, 2 is yaw
+    blackbox_df = pd.read_csv(path, skiprows = 141, 
+                            usecols = ['rcCommand[0]', 'rcCommand[1]', 'rcCommand[2]', 'gyroADC[0]', 'gyroADC[1]', 'gyroADC[2]'])
+    
+
+    pitchArray = np.array(blackbox_df.loc[:, 'rcCommand[1]'].to_numpy(), ndmin = 2).T
+    rollArray = np.array(blackbox_df.loc[:, 'rcCommand[0]'].to_numpy(), ndmin = 2).T
+    yawArray = np.array(blackbox_df.loc[:, 'rcCommand[2]'].to_numpy(), ndmin = 2).T
+    stickArray = np.hstack((pitchArray, rollArray, yawArray))
+
+
+
+    gyro_x = np.array(blackbox_df.loc[:, 'gyroADC[0]'].to_numpy(), ndmin = 2).T
+    gyro_y = np.array(blackbox_df.loc[:, 'gyroADC[1]'].to_numpy(), ndmin = 2).T
+    gyro_z = np.array(blackbox_df.loc[:, 'gyroADC[2]'].to_numpy(), ndmin = 2).T
+
+
+    sos = signal.butter(signal_order, signal_crit_freq, fs = signal_sampling_freq, output = signal_output)
+    filtered = signal.sosfilt(sos, gyro_x)
+    filtered2 = signal.sosfilt(sos, gyro_y)
+    filtered3 = signal.sosfilt(sos, gyro_z)
+
+
+    filtered_gyroArray = np.hstack((filtered, filtered2, filtered3))
+
+    return stickArray, filtered_gyroArray
+
+
+def fit_impulse(x, y):
+    A = np.zeros((N-K+1, K+1))
+    A = linalg.convolution_matrix(x, K, mode = "valid")
+    h = np.linalg.lstsq(A, y, rcond = None)[0]
+    conv = signal.correlate(x, h)
+
+    return A, h, conv
+
+
+
+
+x, y = load_filter_bf_datafile('drone_test_1_BB.csv', 7, 100, 500, 'sos')
+
 
 # set both to 0 in order to not cut anything from graph
 start_t = 0
@@ -20,57 +65,53 @@ K = 800
 N = x.shape[0]
 
 
-def compute_A_h_conv(x, y):
-    A = np.zeros((N-K+1, K+1))
-    A = linalg.convolution_matrix(x, K, mode = "valid")
-    h = np.linalg.lstsq(A, y, rcond = None)[0]
-    conv = signal.correlate(x, h)
 
-    return A, h, conv
+if __name__ == '__main__':
+    ##### Roll to gyro[1] impulse
+    y_2 = y[K-1:, 1]
+    A_2, h_2, conv_2 = fit_impulse(x[:, 0], y[K-1:, 1])
 
 
-
-##### Roll to gyro[1] impulse
-y_2 = y[K-1:, 1]
-A_2, h_2, conv_2 = compute_A_h_conv(x[:, 0], y[K-1:, 1])
-
-
-##### Pitch to gyro [0] impulse
-y_1 = y[K-1:,0]
-A_1, h_1, conv_1 = compute_A_h_conv(x[:, 1], y[K-1:,0])
+    ##### Pitch to gyro [0] impulse
+    y_1 = y[K-1:,0]
+    A_1, h_1, conv_1 = fit_impulse(x[:, 1], y[K-1:,0])
 
 
-##### Yaw to gyro[2] impulse
-y_3 = y[20000+K-1:30000,2]
-A_3, h_3, conv_3 = compute_A_h_conv(x[20000:30000,2], y[20000+K-1:30000,2])
+    ##### Yaw to gyro[2] impulse
+    y_3 = y[20000+K-1:30000,2]
+    A_3, h_3, conv_3 = fit_impulse(x[20000:30000,2], y[20000+K-1:30000,2])
 
 
 
 
 
-################ Plotting Impulses ##########################
+    ################ Plotting Impulses ##########################
 
-temp = 0.001 * np.arange(y_2.size)
-temp2 = 0.001 * np.arange(h_2.size)
-temp3 = 0.001 * np.arange((A_3 @ h_3).size)
-
-
-fig, axs = plt.subplots(3,2)
-fig.suptitle("Stick inputs and Gyro")
+    temp = 0.001 * np.arange(y_2.size)
+    temp2 = 0.001 * np.arange(h_2.size)
+    temp3 = 0.001 * np.arange((A_3 @ h_3).size)
 
 
-
-axs[0,0].plot(temp, A_1 @ h_1, label = "A*h1")
-axs[0,0].plot(temp, y_1, label = "y_pitch")
-axs[0,1].plot(temp2, h_1, label = "impulse1")
+    fig, axs = plt.subplots(3,2)
+    fig.suptitle("Stick inputs and Gyro")
 
 
-axs[1,0].plot(temp, A_2 @ h_2, label = "A*h2")
-axs[1,0].plot(temp, y_2, label = "y_roll")
-axs[1,1].plot(temp2, h_2, label = "impulse2")
 
-axs[2,0].plot(temp3, A_3 @ h_3, label = "A*h3")
-axs[2,0].plot(temp3, y_3, label = "y_yaw")
-axs[2,1].plot(temp2, h_3, label = "impulse3")
+    axs[0,0].plot(temp, A_1 @ h_1, label = "A*h1")
+    axs[0,0].plot(temp, y_1, label = "y_pitch")
+    axs[0,1].plot(temp2, h_1, label = "impulse1")
 
-plt.show()
+
+    axs[1,0].plot(temp, A_2 @ h_2, label = "A*h2")
+    axs[1,0].plot(temp, y_2, label = "y_roll")
+    axs[1,1].plot(temp2, h_2, label = "impulse2")
+
+    axs[2,0].plot(temp3, A_3 @ h_3, label = "A*h3")
+    axs[2,0].plot(temp3, y_3, label = "y_yaw")
+    axs[2,1].plot(temp2, h_3, label = "impulse3")
+
+    plt.show()
+
+
+
+
