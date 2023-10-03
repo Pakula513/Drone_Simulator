e1f427e8f95ce3042cd97614928cfd5428af7187
Pakula513 <akula.prateek@gmail.com>
2023-10-03 11:01:18 -0400

    moved function definition outside main scope

--- impulse_plotter.py
+++ impulse_plotter.py
@@ -7,6 +7,45 @@ import matplotlib.pyplot as plt
 import argparse
 
 
+def load_filter_bf_datafile(signal_order, signal_crit_freq, signal_sampling_freq, signal_output):
+
+    # rcCommand 0 is roll, 1 is pitch, 2 is yaw
+    blackbox_df = pd.read_csv(filepath, skiprows = 141, 
+                            usecols = ['rcCommand[0]', 'rcCommand[1]', 'rcCommand[2]', 'gyroADC[0]', 'gyroADC[1]', 'gyroADC[2]'])
+    
+
+    pitchArray = np.array(blackbox_df.loc[:, 'rcCommand[1]'].to_numpy(), ndmin = 2).T
+    rollArray = np.array(blackbox_df.loc[:, 'rcCommand[0]'].to_numpy(), ndmin = 2).T
+    yawArray = np.array(blackbox_df.loc[:, 'rcCommand[2]'].to_numpy(), ndmin = 2).T
+    stickArray = np.hstack((pitchArray, rollArray, yawArray))
+
+
+
+    gyro_x = np.array(blackbox_df.loc[:, 'gyroADC[0]'].to_numpy(), ndmin = 2).T
+    gyro_y = np.array(blackbox_df.loc[:, 'gyroADC[1]'].to_numpy(), ndmin = 2).T
+    gyro_z = np.array(blackbox_df.loc[:, 'gyroADC[2]'].to_numpy(), ndmin = 2).T
+
+
+    sos = signal.butter(signal_order, signal_crit_freq, fs = signal_sampling_freq, output = signal_output)
+    filtered = signal.sosfilt(sos, gyro_x)
+    filtered2 = signal.sosfilt(sos, gyro_y)
+    filtered3 = signal.sosfilt(sos, gyro_z)
+
+
+    filtered_gyroArray = np.hstack((filtered, filtered2, filtered3))
+
+    return stickArray, filtered_gyroArray
+
+
+def fit_impulse(x, y):
+    A = np.zeros((N-K+1, K+1))
+    A = linalg.convolution_matrix(x, K, mode = "valid")
+    h = np.linalg.lstsq(A, y, rcond = None)[0]
+    conv = signal.correlate(x, h)
+
+    return A, h, conv
+
+
 
 if __name__ == '__main__':
 
@@ -17,47 +56,6 @@ if __name__ == '__main__':
 
     if not filepath:
         filepath = "drone_test_1_BB.csv"
-
-
-    def load_filter_bf_datafile(signal_order, signal_crit_freq, signal_sampling_freq, signal_output):
-
-        # rcCommand 0 is roll, 1 is pitch, 2 is yaw
-        blackbox_df = pd.read_csv(filepath, skiprows = 141, 
-                                usecols = ['rcCommand[0]', 'rcCommand[1]', 'rcCommand[2]', 'gyroADC[0]', 'gyroADC[1]', 'gyroADC[2]'])
-        
-
-        pitchArray = np.array(blackbox_df.loc[:, 'rcCommand[1]'].to_numpy(), ndmin = 2).T
-        rollArray = np.array(blackbox_df.loc[:, 'rcCommand[0]'].to_numpy(), ndmin = 2).T
-        yawArray = np.array(blackbox_df.loc[:, 'rcCommand[2]'].to_numpy(), ndmin = 2).T
-        stickArray = np.hstack((pitchArray, rollArray, yawArray))
-
-
-
-        gyro_x = np.array(blackbox_df.loc[:, 'gyroADC[0]'].to_numpy(), ndmin = 2).T
-        gyro_y = np.array(blackbox_df.loc[:, 'gyroADC[1]'].to_numpy(), ndmin = 2).T
-        gyro_z = np.array(blackbox_df.loc[:, 'gyroADC[2]'].to_numpy(), ndmin = 2).T
-
-
-        sos = signal.butter(signal_order, signal_crit_freq, fs = signal_sampling_freq, output = signal_output)
-        filtered = signal.sosfilt(sos, gyro_x)
-        filtered2 = signal.sosfilt(sos, gyro_y)
-        filtered3 = signal.sosfilt(sos, gyro_z)
-
-
-        filtered_gyroArray = np.hstack((filtered, filtered2, filtered3))
-
-        return stickArray, filtered_gyroArray
-
-
-    def fit_impulse(x, y):
-        A = np.zeros((N-K+1, K+1))
-        A = linalg.convolution_matrix(x, K, mode = "valid")
-        h = np.linalg.lstsq(A, y, rcond = None)[0]
-        conv = signal.correlate(x, h)
-
-        return A, h, conv
-
-
 
 
     x, y = load_filter_bf_datafile(7, 100, 500, 'sos')
